962734217d8e3f833f31b5d562e576505a5393f6
Steven-LR <stevenlawrence.r@gmail.com>
2021-02-11 11:33:22 -0500

    function read_tilt can now read all types of files.

--- R/read_tilt.R
+++ R/read_tilt.R
@@ -3,11 +3,19 @@ library(tidyverse)
 
 ## reading in csv tiltfiles
 
-read_tilt <- function(path, study){
-  files_names<- paste("./Autonomics raw data/",study,list.files(paste0("./Autonomics raw data/",study)),sep="")
-  files_names<-list.files(paste0("./Autonomics raw data/",study))
-  data = tibble(names = files_names) %>%
-    mutate(contents = map(names, ~read_csv(file.path(paste0("./Autonomics raw data/",study), .))))
+read_tilt <- function(path,file_type){
 
-  data
+  if (!requireNamespace(c("readr","dplyr"), quietly = TRUE)) {
+    stop("Packages readr and dplyr are needed to run this function.",
+         call. = FALSE)
+  }
+
+  files_names<- paste(path, list.files(paste0(path)),sep="")
+  files_names<-list.files(paste0(path))
+
+  # Creats a table of the file names and their contents
+  df = tibble(names = files_names) %>%
+    mutate(contents = map(names, ~file_type(file.path(paste0(path), .))))
+
+  df
 }
